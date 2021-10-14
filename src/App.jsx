13b745e73376a7a7b388e9aea517c59dcfe4f31f
Conquer <conquer@pop-os.localdomain>
2021-10-14 18:42:01 -0300

    fixed merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,8 @@ import { Footer } from "./components/Footer/Index";
 import { Header } from "./components/Header/Index";
 import { Home } from "./pages/Home/Index";
 
-<<<<<<< HEAD
-import { GlobalStyle } from "./styles/global";
-import ContainerTempTest from "./components/ContainerTempTest/Index";
-=======
 import { GlobalStyle } from "./styles/global.js";
 import DicasUteis from "./components/DicasUteis/Index";
->>>>>>> 2e44487fdf62df60527e8a48738ec83e65ff1b9a
 
 export function App() {
 	return (
@@ -17,11 +12,6 @@ export function App() {
 			<GlobalStyle />
 			<Header />
 			<Home />
-<<<<<<< HEAD
-			<ContainerTempTest />
-			<ContainerSectionCard />
-=======
->>>>>>> 2e44487fdf62df60527e8a48738ec83e65ff1b9a
 			<Footer />
 		</>
 	)
